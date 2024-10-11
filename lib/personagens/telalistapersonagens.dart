e099f18e328199c23cb2ebba2cc01ae3e3930b2e
blaiton <guiyzzzgabriel@gmail.com>
2024-10-11 09:07:21 -0300

    aaa

---
+++ lib/personagens/telalistapersonagens.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Telalistapersonagens extends StatelessWidget {
+  const Telalistapersonagens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
